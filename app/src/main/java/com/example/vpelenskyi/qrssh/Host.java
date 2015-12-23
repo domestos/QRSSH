6e549c21036cede48b38f4bf67613b627ed40c86
v.pelenskyi <elBarto2006@ukr.net>
2015-12-23 18:00:42 +0200

    Add setOnClickListener on button SAVE

---
+++ app/src/main/java/com/example/vpelenskyi/qrssh/Host.java
@@ -0,0 +1,85 @@
+package com.example.vpelenskyi.qrssh;
+
+/**
+ * Created by v.pelenskyi on 23.12.2015.
+ */
+public class Host {
+
+    private String alias;
+    private String host;
+    private String username;
+    private String password;
+    private int port;
+    private String os;
+
+    public Host(String alias, String host, int port, String username, String password,  String os) {
+        this.alias = alias;
+        this.host = host;
+        this.username = username;
+        this.password = password;
+        this.port = port;
+        this.os = os;
+    }
+
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Host{" +
+                "alias='" + alias + '\'' +
+                ", host='" + host + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", port=" + port +
+                ", os='" + os + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/com/example/vpelenskyi/qrssh/NewHost.java
+++ app/src/main/java/com/example/vpelenskyi/qrssh/NewHost.java
@@ -5,7 +5,10 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by v.pelenskyi on 22.12.2015.
@@ -13,43 +16,72 @@ import android.widget.EditText;
 public class NewHost extends AppCompatActivity {
 
 
-
+    private TextInputLayout tilAlias, tilHost, tilPort, tilUsername, tilPassword;
+    private EditText alias, host, port, username, password;
+    private Button btnSaveHost;
+    private String os = "";
+    //  Host newhost =null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_host_activity);
 
-        ActionBar toolbar =  getSupportActionBar();
+
+        ActionBar toolbar = getSupportActionBar();
         toolbar.setDisplayHomeAsUpEnabled(true);
         toolbar.setTitle("Add New HOST");
         intTextInputLayout();
+
+//        btnSaveHost.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                newhost = new Host(
+//                        alias.getText().toString(),
+//                        host.getText().toString(),
+//                        Integer.valueOf(port.getText().toString()),
+//                        username.getText().toString(),
+//                        password.getText().toString(),
+//                        os
+//                );
+//
+//                Toast.makeText(getApplicationContext(), newhost.toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
 
 //        Toolbar toolbarNewHost = (Toolbar) findViewById(R.id.toolbarNewHost);
 //        setSupportActionBar(toolbarNewHost);
     }
 
+    public void myClick(View v) {
+
+        Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_SHORT).show();
+
+    }
+
+
     private void intTextInputLayout() {
 
-        TextInputLayout tilAlias = (TextInputLayout) findViewById(R.id.aliasInputLayout);
-        EditText alias = (EditText) tilAlias.findViewById(R.id.alias);
+        tilAlias = (TextInputLayout) findViewById(R.id.aliasInputLayout);
+        alias = (EditText) tilAlias.findViewById(R.id.alias);
         tilAlias.setHint(getString(R.string.etAlias));
 
-        TextInputLayout tilHost = (TextInputLayout) findViewById(R.id.hostInputLayout);
-        EditText host = (EditText) tilHost.findViewById(R.id.host);
+        tilHost = (TextInputLayout) findViewById(R.id.hostInputLayout);
+        host = (EditText) tilHost.findViewById(R.id.host);
         tilHost.setHint(getString(R.string.hintHost));
 
-        TextInputLayout tilPort = (TextInputLayout) findViewById(R.id.portInputLayout);
-        EditText port = (EditText) tilPort.findViewById(R.id.port);
+        tilPort = (TextInputLayout) findViewById(R.id.portInputLayout);
+        port = (EditText) tilPort.findViewById(R.id.port);
         tilPort.setHint(getString(R.string.hintPort));
 
-        TextInputLayout tilUsername = (TextInputLayout) findViewById(R.id.usernameInputLayout);
-        EditText username = (EditText) tilUsername.findViewById(R.id.username);
+        tilUsername = (TextInputLayout) findViewById(R.id.usernameInputLayout);
+        username = (EditText) tilUsername.findViewById(R.id.username);
         tilPort.setHint(getString(R.string.hintUsername));
 
-        TextInputLayout tilPassword = (TextInputLayout) findViewById(R.id.usernameInputLayout);
-        EditText password = (EditText) tilPassword.findViewById(R.id.password);
+        tilPassword = (TextInputLayout) findViewById(R.id.usernameInputLayout);
+        password = (EditText) tilPassword.findViewById(R.id.password);
         tilPort.setHint(getString(R.string.hintPassword));
 
     }
